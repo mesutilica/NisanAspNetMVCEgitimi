7803b555b274a1cb27d4689bae2a70b6c6633ac4
Mesut ILICA <mesut.ilica@gmail.com>
2024-09-03 07:17:03 +0300

    Add project files.

---
+++ NisanAspNetMVCEgitimi/Controllers/MVC11CookieController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NisanAspNetMVCEgitimi.Models;
+
+namespace NisanAspNetMVCEgitimi.Controllers
+{
+    public class MVC11CookieController : Controller
+    {
+        private readonly UyeContext _context;
+
+        public MVC11CookieController(UyeContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CookieOlustur(string kullaniciAdi, string sifre)
+        {
+            var kullanici = _context.Uyeler.FirstOrDefault(u => u.KullaniciAdi == kullaniciAdi && u.Sifre == sifre);
+            if (kullanici != null)
+            {
+
+            }
+            return View();
+        }
+    }
+}
